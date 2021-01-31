6334e7f46753452a92492a0834174619d5be06ad
Gorka <gorka@veiss.com>
2021-01-31 18:35:33 +0100

    Livewire: incluir y crear un primer componente de ejemplo

---
+++ app/Http/Livewire/LandingPage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LandingPage extends Component
+{
+    
+    public $email;
+
+    public function render()
+    {
+        return view('livewire.landing-page');
+    }
+
+    
+    public function subscribe()
+    {
+        \Log::debug("email: " . $this->email);
+    }
+
+}
